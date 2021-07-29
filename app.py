828228cf16d1ea80007e466d8b6d9ded8eda91f8
Denisha Perez <78589229+DenishaYP-creator@users.noreply.github.com>
2021-07-28 20:03:33 -0400

    changing app py file

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import numpy as np
 
 
 
-
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 # IMAGES_FOLDER = os.path.join('static', 'images')
@@ -22,14 +21,21 @@ bootstrap = Bootstrap(app)
 # Endpoint-GET Request for index.html files
 @app.route('/')
 def index():
-#     full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'THEQUESTION.png')
+    #data = db.table.find_one()
     return render_template('index.html')
-#     return render_template("index.html", question_image = full_filename)
+    #render_template("index.html", data=table)
 
-  
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404.html')
+
+# app.config['SECRET_KEY'] = 'top_secret'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
+# db = SQLAlchemy(app)
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/WomenInTech"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
 
 @app.errorhandler(404)
 def not_found(e):
@@ -37,17 +43,7 @@ def not_found(e):
 
 
 if __name__ == '__main__':
+    # db.create_all()
     app.run(debug=True)
-
-
-
-@app.route(/api/data)
-def data():
-    
-    return render_template('about.html')
-
-
-
-
-
+    # app.run_server(debug=True)
 
